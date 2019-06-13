9f6c2a4b4bdbb12718f861652fc5345499a24b93
Todor <tm@opinodo.com>
2019-06-14 00:48:56 +0200

    models, migrations and Procfile

---
+++ app/Http/Controllers/CinemaHallController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CinemaHall;
+use Illuminate\Http\Request;
+
+class CinemaHallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CinemaHall  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CinemaHall $cinema)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CinemaHall  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CinemaHall $cinema)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CinemaHall  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CinemaHall $cinema)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CinemaHall  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CinemaHall $cinema)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_06_13_211124_create_projections_and_movies_tables.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectionsAndMoviesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projections', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('movie_id');
+            $table->unsignedInteger('cinema_hall_id');
+            $table->dateTime('start_at');
+            $table->double('ticket_price');
+            $table->timestamps();
+        });
+
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('length');
+            $table->timestamps();
+        });
+
+        Schema::create('cinema_halls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hall_name');
+            $table->integer('number_of_rows');
+            $table->integer('seats_in_row');
+            $table->timestamps();
+        });
+
+        Schema::create('genres', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('genre_movie', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('genre_id');
+            $table->unsignedInteger('movie_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projections');
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('genre_movie');
+        Schema::dropIfExists('genres');
+        Schema::dropIfExists('cinema_halls');
+    }
+}
